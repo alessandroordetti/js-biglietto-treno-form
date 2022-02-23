c10cfdf2d3a61da6b4b578d2e8c56110053d8088
alessandroordetti <alessandro.ordetti@gmail.com>
2022-02-23 19:10:08 +0100

    Ticket-price

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,4 +1,33 @@
-document.querySelector("button").addEventListener('click', function(){
-    
+document.getElementById("enter-button").addEventListener('click', function(){
 
-})
+    /*prendo il valore dell'id user-name*/
+    let nominativo = document.getElementById('user-name').value;
+
+    document.getElementById('traveller-name').innerHTML = nominativo;
+
+    let km = document.getElementById("user-km");
+    let kilometri = parseInt(km.value);
+
+    let ticket = (kilometri * 0.27);
+
+    let age = document.getElementById("età").value;
+
+
+    if (age == "Minorenne") {
+        ticket - (ticket - (ticket * 0.17)).toFixed(2);
+    } else if (age == "Over65") {
+        ticket - (ticket -(ticket * 0.33)).toFixed(2);
+    } else {
+        ticket = ticket.toFixed(2);
+    }
+
+    document.getElementById('ticket-price').innerHTML = ticket + "€";
+
+    if ((age == "Minorenne") || (age == "Over65")) {
+        document.getElementById("ticket-type").innerHTML = "Biglietto scontato"
+    } else {
+        document.getElementById("ticket-type").innerHTML = "Biglietto regolare"
+    }
+
+
+});
